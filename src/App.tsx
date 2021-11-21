ffd664ec0ef15be7c87a88bfd477acfc32c7ede6
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-11-21 19:49:47 -0400

    chore: add react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
 import { ThemeProvider } from 'styled-components';
-
+import { ToastContainer, Flip } from 'react-toastify';
 import { Home } from './pages/Home';
 
 const App: React.FC = () => (
   <BrowserRouter>
     <ThemeProvider theme={theme}>
+      <ToastContainer transition={Flip} limit={1} />
       <GlobalStyles />
 
       <Routes>
