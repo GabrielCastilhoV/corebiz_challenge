72173b1103fb67961e6120e0d3b75f7ec1c0c682
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-11-21 18:39:06 -0400

    feat: connection with api and add productSlider in home page

--- src/components/layouts/Home/index.tsx
+++ src/components/layouts/Home/index.tsx
@@ -2,10 +2,41 @@ import { Header } from '../../../components/modules/Header';
 import { BannerSlider } from '../../../components/elements/BannerSlider';
 import { Heading } from '../../../components/elements/Heading';
 import bannersMock from '../../../components/elements/BannerSlider/mock';
+import { ProductSlider } from '../../../components/elements/ProductSlider';
 
 import * as S from './styles';
+import { useEffect, useState } from 'react';
+import api from '../../../services/api';
+
+type Installment = {
+  quantity: number;
+  value: number;
+};
+
+export type ProductProps = {
+  productId: number;
+  productName: string;
+  stars: number;
+  imageUrl: string;
+  listPrice?: number;
+  price: number;
+  installments: Installment[];
+  qtd?: number;
+};
 
 export const HomeLayout = () => {
+  const [products, setProducts] = useState<ProductProps[]>([]);
+
+  useEffect(() => {
+    async function loadProducts() {
+      const response = await api.get<ProductProps[]>('/products');
+
+      setProducts(response.data);
+    }
+
+    loadProducts();
+  }, [products]);
+
   return (
     <S.Wrapper>
       <Header />
@@ -16,6 +47,7 @@ export const HomeLayout = () => {
 
       <S.ProductWrapper>
         <Heading name="Mais Vendidos" />
+        <ProductSlider products={products} />;
       </S.ProductWrapper>
     </S.Wrapper>
   );
