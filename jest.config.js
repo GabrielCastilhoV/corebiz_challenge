0b1eba8b2c22d86305ed99b5b8626332f6a7b0b0
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-11-21 10:36:01 -0400

    chore: add jest and react testing library

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/'],
+  transform: {
+    '^.+\\.(js|ts|tsx)$': 'babel-jest',
+    '.+\\.(svg|css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+  },
+};
