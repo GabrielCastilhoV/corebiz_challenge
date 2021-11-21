deda5b5f8cd3f8bd0210f6c18931c3b122fed472
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-11-21 15:03:38 -0400

    feat: add banner from home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,18 @@
 import { Header } from '../components/modules/Header';
+import { BannerSlider } from '../components/elements/BannerSlider';
+
+import bannersMock from '../components/elements/BannerSlider/mock';
+
+import * as S from '../styles/home';
 
 export const Home = () => {
   return (
-    <>
+    <S.Wrapper>
       <Header />
-      <h1>CoreBiz Challenge</h1>)
-    </>
+
+      <S.SectionBanner>
+        <BannerSlider items={bannersMock} />
+      </S.SectionBanner>
+    </S.Wrapper>
   );
 };
---
+++ src/styles/home.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SectionBanner = styled.section`
+  margin-top: 2rem;
+  width: 100%;
+`;
