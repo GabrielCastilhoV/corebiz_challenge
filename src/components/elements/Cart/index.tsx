8443be5a21cc437b4b1c061d55e289bb2ec17a0d
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-11-23 08:04:37 -0400

    fix: change in method quantity of products

--- src/components/elements/Cart/index.tsx
+++ src/components/elements/Cart/index.tsx
@@ -3,14 +3,21 @@ import * as S from './styles';
 import { BsCart4 } from 'react-icons/bs';
 
 import { useCart } from '../../../contexts/Cart';
-import { SetStateAction } from 'react';
+import { SetStateAction, useEffect, useState } from 'react';
 
 type CartProps = {
   setOpenCart: React.Dispatch<SetStateAction<boolean>>;
 };
 
 export const Cart = ({ setOpenCart }: CartProps) => {
-  const { qtd } = useCart();
+  const { products } = useCart();
+
+  const [qtd, setQtd] = useState(0);
+
+  useEffect(() => {
+    const total = products?.reduce((sumTotal, { qtd }) => sumTotal + qtd!, 0);
+    setQtd(total);
+  }, [products]);
 
   return (
     <S.Wrapper aria-label="Open cart" onClick={() => setOpenCart(true)}>
